1056442db12109fde4b82056ea2dba74203ae502
Abhishek <abhirajp075@gmail.com>
2022-07-02 22:17:47 +0530

    modifided

--- Cycle/18_Link_search.cpp
+++ Cycle/18_Link_search.cpp
@@ -24,17 +24,17 @@ public:
     }
     void insert_node(int value){
         node* new_node=new node(value);
-        cout<<"Constructed";
+        // cout<<"Constructed";
         if(start==NULL){
             start=new_node;
-            cout<<"Entered start"<<endl;
+            // cout<<"Entered start"<<endl;
 
         }
-        cout<<"Hai";
+        // cout<<"Hai";
         node* ptr=start;
         while (ptr->link!=NULL){
             ptr=ptr->link;
-            cout<<"Entered link "<<endl;
+            // cout<<"Entered link "<<endl;
 
         }
         ptr->link=new_node;
@@ -49,15 +49,22 @@ public:
             }
             start=start->link;
             count+=1;
-            cout<<"HAi at search list" <<endl;
+            // cout<<"HAi at search list" <<endl;
         }
     }
 };
 int main(){
     linked_list list;
-    
-    list.insert_node(12);
-    list.insert_node(13);
-    list.search_list(13);
+    int insert_info,no_nodes,search_key;
+    cout<<"Enter the number of nodes to be created : "<<endl;
+    cin>>no_nodes;
+    for(int i=0;i<no_nodes;i++){
+        cout<<"Enter the number to be inserted in the list : "<<endl;
+        cin>>insert_info;
+        list.insert_node(insert_info);
+    }
+    cout<<"Enter the number to be searched in the list : "<<endl;
+    cin>>search_key;
+    list.search_list(search_key);
     return 0;
 }
