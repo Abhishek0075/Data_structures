f8c186c2512c18260c4a85991ef6422d3b35b659
Abhishek <abhirajp075@gmail.com>
2022-07-05 12:51:56 +0530

    change

--- Cycle/IN10_merging_sort.cpp
+++ Cycle/IN10_merging_sort.cpp
@@ -1,21 +1,32 @@
 #include<iostream>
 using namespace std;
 
-// void merge(int arr1[],int element1,int low1,int arr2[],int element2,int low2,int final[],int lowFINAl){
+void merge(int arr1[],int element1,int lb1,int arr2[],int element2,int lb2,int final[],int lbFINAl){
+    // int start1=lb1,start2=lb2,startFinal=lbFINAl;
+    int upper1=lb1+element1-1,upper2=lb2+element2-1;
 
-//     int start1=low1,start2=low2,startFinal=lowFINAl;
-//     int upper1=low1+element1-1,int upper2=low2+element2-1;
-
-//     while(start1<=upper1 and start2<=upper2){
-//         if (start1>upper1){
-//             for(int k=0;k<=(upper2-start2);k++){
-//                 final[startFinal]=arr2[k+start2];
-//             }
-//         }else{
-            
-//         }
-//     }
-// }
+    while(lb1<=upper1 and lb2<=upper2){
+        if (lb1<lb2){
+            final[lbFINAL]=arr1[lb1];
+            lb1+=1;
+            lbFINAL+=1;
+        }else{
+            final[lbFINAL]=arr2[lb2];
+            lb2+=1;
+            lbFINAL+=1;
+        }
+    }
+    if(lb1>upper1){
+        for(int i=0;i<upper2-lb2){
+            final[lbFINAl+i]=arr2[lb2+i];
+        }
+    }else{
+        for(int i=0;i<upper1-lb1){
+            final[lbFINAl+i]=arr1[lb1+i];
+        }
+    }
+}
+void merge_pass()
 int main(){
 
     return 0;
