6a3a23220832f906303b76feb177cbdb7b7dbbf5
Abhishek <abhirajp075@gmail.com>
2022-07-07 23:46:26 +0530

    new

--- Cycle/tree_binary.cpp
+++ Cycle/tree_binary.cpp
@@ -20,40 +20,46 @@ public:
     }
 };
 class tree{
-    node* start;
+    node* start=NULL;
 public:
-    tree(){
-        start=NULL;
-    }
+    tree(){}
     void insert_node(int num){
-        node* new_node=new node(num);
-        if(new_node==NULL){
+        int count;
+        node* newNode=new node(num);
+        if(newNode==NULL){
             cout<<"Over Flow"<<endl;
             return;
         }
         if(start==NULL){
-            start=new_node;
+            start=newNode;
             return;
         }
         node* ptr=start;
-        while(ptr->rightSide!=NULL or ptr->leftSide!=NULL){
-            if(new_node->info<ptr->info){
+        while(ptr->leftSide!=NULL and ptr->rightSide!=NULL){
+            newNode->parent=ptr;
+            if(newNode->info<ptr->info){
                 ptr=ptr->leftSide;
             }else{
                 ptr=ptr->rightSide;
             }
         }
-        if(ptr->leftSide==NULL){
-            ptr->leftSide=new_node;
+        if(newNode->info<ptr->info){
+            ptr->leftSide=newNode;
+        }else{
+            ptr->rightSide=newNode;
         }
-        if(ptr->rightSide==NULL){
-            ptr->rightSide=new_node;
-        }
-        new_node->rightSide=NULL;
-        new_node->leftSide=NULL;
+        newNode->parent=ptr->parent;
+        newNode->rightSide=NULL;
+        newNode->leftSide=NULL;
     }
 };
 int main(){
-
+    tree t;
+    t.insert_node(10);
+    t.insert_node(9);
+    t.insert_node(31);
+    t.insert_node(30);
+    t.insert_node(8);
+    t.insert_node(50);
     return 0;
 }
