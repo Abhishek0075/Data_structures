4622870865948a44031c472f49fefb5c98dc35de
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2022-07-13 16:23:32 +0530

    Update 2_linear_search_iterative.cpp

--- Cycle/2_linear_search_iterative.cpp
+++ Cycle/2_linear_search_iterative.cpp
@@ -1,36 +1,40 @@
 #include<iostream>
 using namespace std;
-class search{
-    public:
-      void linear_search_iterative(int arr[10],int key){
-    	int position=-1;
-    	for(int i=0;i<=10;i++){
-    		if(arr[i]==key){
-    			position=i;
-    			break;
-    		}
-    	}
-    	if(position==-1){
-    	    cout<<key<<" not in the array"<<endl;
-    	}else{
-    	    cout<<key<<" found at "<<(position+1)<<"th postion"<<endl;
-    	}
-    }
-};
+
+void linear_search_iterative(int arr[],int no_element,int key){
+	int position=-1;
+	for(int i=0;i<=no_element;i++){
+		if(arr[i]==key){
+			position=i;
+			break;
+		}
+	}
+	if(position==-1){
+		cout<<key<<" not in the array"<<endl;
+	}else{
+		cout<<key<<" found at "<<(position+1)<<"th postion"<<endl;
+	}
+}
+
 
 int main(){
-	search A;
-	int key;
-	int a[10]={10,9,4,3,2,11,5,7,2,1};
+	int key,no_element;
+	cout<<"Enter the number of elements in the array : ";
+	cin>>no_element;
+	int a[no_element];
+	cout<<"Enter the elements of the array ";
+	for(int i=0;i<no_element;i++){
+		cin>>a[i];
+	}
 	cout<<"The array = ";
-	for(int i=0;i<10;i++){
+	for(int i=0;i<no_element;i++){
 		cout<<a[i]<<" ";
 	}
 	cout<<endl;
 	cout<<"Enter the key to be searched "<<endl;
 	cin>>key;
 	cout<<"Result : "<<endl;
-	A.linear_search_iterative(a,key);
+	linear_search_iterative(a,key);
 	return 0;
 }
 
