55956a08a26d54f0908916efabfce4e927848bce
Abhishek <abhirajp075@gmail.com>
2022-07-16 10:25:03 +0530

    bubble_sort corrected

--- Cycle/8_bubble_sort.cpp
+++ Cycle/8_bubble_sort.cpp
@@ -1,30 +1,38 @@
 #include<iostream>
 using namespace std;
 void bubble_sort(int arr[],int no_element){
-    int temp;
-    for(int i=0;i<no_element-1;i++){
-        for(int j=i+1;j<no_element;j++){
-            if (arr[i]>arr[j]){
-                temp=arr[j];
-                arr[j]=arr[i];
+    int temp,ele=no_element;
+    while(no_element>1){
+        for(int i=0;i<no_element-1;i++){
+            if(arr[i+1]<arr[i]){
+                temp=arr[i+1];
+                arr[i+1]=arr[i];
                 arr[i]=temp;
             }
         }
+        no_element=no_element-1;
     }
-    for(int i=0;i<no_element;i++){
+    for(int i=0;i<ele;i++){
         cout<<arr[i]<<" ";
     }
-    cout<<endl;
+    
 }
 int main(){
-    int array[7]={2,1,9,11,8,17,10};
+    int no_element;
+    cout<<"Enter the number of elements in the array : ";
+    cin>>no_element;
+    int array[no_element];
+    cout<<"Enter the elements in the array : "<<endl;
+    for(int i=0;i<no_element;i++){
+        cin>>array[i];
+    }
     cout<<endl;
     cout<<"Before bubble sort : "<<endl;
-    for(int i=0;i<7;i++){
+    for(int i=0;i<no_element;i++){
         cout<<array[i]<<" ";
     }
     cout<<endl;
     cout<<"After bubble sort : "<<endl;
-    bubble_sort(array,7);
+    bubble_sort(array,no_element);
     return 0; 
 }
