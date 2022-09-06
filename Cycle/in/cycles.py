789d7e4dccec015f1a355aba2ce580d4f9308524
abhirajp075@gmail.com <abhirajp075@gmail.com>
2022-09-06 10:59:23 +0530

    new

---
+++ Cycle/in/cycles.py
@@ -0,0 +1,33 @@
+
+#  Inserting and deleting an element from an Array
+#  Linear Search
+#  Binary Search
+#  Ternary Search
+#  Interpolation Search
+#  Fibonacci Search
+#  Selection Sort
+#  Bubble Sort
+#  Insertion sort
+#  Merge sort
+#  Substring Extraction
+#  Pattern Matching
+#  Insertion of String, Deletion of string and Replacement of substring as
+    # each functions in a program.
+#  Transpose of a sparse matrix
+#  Addition of sparse matrix
+#  Saddle point
+#  Program to add two polynomials using array.
+#  Searching an item in linked list.
+#  Insertion of a node to a sorted link list.
+#  Merge two sorted list into a single sorted list.
+#  Deletion from the linked list
+#  Program to add two polynomial using linked list.
+#  Implement Insertion and Deletion operation for Circular Header List.
+#  Searching an item in the Two-way linked list.
+#  Deletion and insertion of node in two way list
+#  Push and pop in stack using array.
+#  Push and pop in stack using linkedlist.
+#  Create a Binary search tree and perform traversal(search), insert and
+#  delete on the tree.
+#  Quick Sort.
+#  Program to dequeue and enqueue in queue data structure.
